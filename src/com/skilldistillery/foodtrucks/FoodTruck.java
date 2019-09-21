18c78a9fe0cd62fdbc0a7e5e9c2c31dc91d83596
Andrew Wong <ndrwchkwng@yahoo.com>
2019-09-20 21:38:02 -0600

    Fields Get Set

--- src/com/skilldistillery/foodtrucks/FoodTruck.java
+++ src/com/skilldistillery/foodtrucks/FoodTruck.java
@@ -1,5 +1,52 @@
 package com.skilldistillery.foodtrucks;
 
 public class FoodTruck {
+	private int uniqueId;
+	private String name;
+	private String foodType;
+	private double rating;
+	
+	public FoodTruck() {
+	}
 
+	public FoodTruck(int uniqueId, String name, String foodType, double rating) {
+		super();
+		this.uniqueId = uniqueId;
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+	}
+
+	public int getUniqueId() {
+		return uniqueId;
+	}
+
+	public void setUniqueId(int uniqueId) {
+		this.uniqueId = uniqueId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	
 }

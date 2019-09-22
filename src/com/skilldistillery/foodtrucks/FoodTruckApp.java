155cd00c5ddf9cf67562375307d06486754a8730
Andrew Wong <ndrwchkwng@yahoo.com>
2019-09-22 03:15:32 -0600

    Avg Rating functional

--- src/com/skilldistillery/foodtrucks/FoodTruckApp.java
+++ src/com/skilldistillery/foodtrucks/FoodTruckApp.java
@@ -8,19 +8,17 @@ public class FoodTruckApp {
 
 	Scanner kb = new Scanner(System.in);
 	static boolean isChoose;
+	FoodTruck[] foodTrucks = new FoodTruck [5];
 	
+			
 	public static void main(String[] args) {
 		
 		
-		FoodTruck[] foodTrucks = new FoodTruck[5];
 		FoodTruckApp methodCall = new FoodTruckApp();
-		FoodTruck fT1 = new FoodTruck();
-		FoodTruck fT2 = new FoodTruck(002, "DosBrosTacos", "Mexican", 4);
+//		FoodTruck fT1 = new FoodTruck();
+//		FoodTruck fT2 = new FoodTruck(002, "DosBrosTacos", "Mexican", 4);
 		
-		
-//		foodTrucks[0] =  ;
-		
-		System.out.println(fT2.getName());
+//		System.out.println(fT2.getName());
 		
 		do {
 			isChoose = true;
@@ -30,55 +28,96 @@ public class FoodTruckApp {
 		
 		} while (isChoose == true) ;
 		
-		System.out.println("End of program");
+		System.out.println("End of program, have a nice day");
 		
 	}
 	
 	void menuOptions () {
-		int choice = 0;
+		int choice = kb.nextInt();
 			
 			System.out.println("");
-			choice = kb.nextInt();
 		
 			switch (choice) {
 				
 				case 1:
-					System.out.println("Enter name of food truck: ");
-					String name = kb.next();
-					System.out.println("What is the food type?: ");
-					String foodType = kb.next();
-					System.out.println("Rating from 1-5: ");
-					int rating = kb.nextInt();
-					System.out.println();
+					
+					newEntry();
 					break;
 				
 				case 2:
-//					System.out.println(FoodTruck.getName());
 					
-				case 3:
-				
-				case 4:
-				
-				case 5:
-					isChoose = false;
+					listAll();
 					break;
-						
+
+				case 3: //see avg rating of food trucks
+				
+					printFoodTruckRating();
+					break;
+				case 4:	//display highest rated food truck
+				
+				case 5: //quit program
+					isChoose = false;
+					break;			
 				}
 			}		
 		
-	public void printMenu() {
+	void printMenu() {
 		System.out.println("1: Add foodtruck");
 		System.out.println("2: List all foodtrucks");
 		System.out.println("3: Average rating of foodtrucks");
 		System.out.println("4: Display highest-rated foodtruck");
 		System.out.println("5: Quit program");
 	}
+	void printFoodTruckRating() {
+		double total = 0.0, avg = 0.0;
+		int i = 0, ratingCounter = 0;
+		for (i = 0; i < foodTrucks.length ; i++) {
+		
+			if (foodTrucks[i].getRating() > 0.5) {
+				total = total + foodTrucks[i].getRating();
+				ratingCounter ++;
+			}
+		}
+		avg = total / (ratingCounter);
+		System.out.println("Average: " + avg); 
+	}
+	void listAll() {
+		for (int i = 0; i < foodTrucks.length ; i++)
+		
+			if (foodTrucks[i].getName() != null) {
+			System.out.println("Name: " + foodTrucks[i].getName() + 
+				"  Food Type: " + foodTrucks[i].getFoodType() +
+				"  Rating: " + foodTrucks[i].getRating() );
+			}
+	}
 	
-	
-	public void newEntry() {
-//		uniqueId
-//		name
-//		food type
-//		rating
+	void newEntry() {
+		
+		// initializer so i don't get the null pointer exception
+		for (int count = 0; count < foodTrucks.length; count++) {
+			foodTrucks[count] = new FoodTruck();
+			foodTrucks[count].setUniqueId(0+count);
+		}
+		
+		for (int i = 0; i < foodTrucks.length ; i++) {
+			System.out.println("Enter name of food truck: ");
+			String name = kb.next();
+		if (name.equals("quit")) 
+		break;
+			System.out.println("What is the food type?: ");
+			String foodType = kb.next();
+			System.out.println("Rating from 1-5: ");
+			int rating = kb.nextInt();
+			
+		
+			foodTrucks[i].setUniqueId(i+1);
+			foodTrucks[i].setName(name);
+			foodTrucks[i].setFoodType(foodType);
+			foodTrucks[i].setRating(rating);
+			
+			System.out.print("assigning inputed values for");
+			System.out.println(" unique Id: " + foodTrucks[i+1].getUniqueId());
+			System.out.println("enter next food truck entry or type quit");
+		}
 	}
 }
